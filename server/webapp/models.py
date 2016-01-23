56c2c037e9fa270dde37e4c7bcef5eb73c059e09
Steve King, Jr <stevekx86@gmail.com>
2016-01-23 03:39:25 -0700

    SUCH INSIGHT

--- server/webapp/models.py
+++ server/webapp/models.py
@@ -3,10 +3,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(object):
-    def __init__(self, username, password, phone_number):
-        self.username = username
+
+    def __init__(self):
+        self.username = ""
+        self.password_hash = ""
+        self.phone_number = ""
+
+    def set_password(self, password):
         self.password_hash = generate_password_hash(password, method="pbkdf2:sha256", salt_length=32)
-        self.phone_number = phone_number
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
@@ -17,4 +21,10 @@ class User(object):
 
     @classmethod
     def get_from_db(cls, username):
-        redis_db.hmget(username, ["password_hash", "phone_number"])
+        user_dict = redis_db.hmget(username, ["password_hash", "phone_number"])
+        fetched_user = User()
+        fetched_user.username = username
+        fetched_user.password_hash = user_dict["password_hash"]
+        fetched_user.phone_number = user_dict["phone_number"]
+        return fetched_user
+
