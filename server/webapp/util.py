c0dffa16ddb17cc22464fbc67abb8af79cea250b
Steve King, Jr <stevekx86@gmail.com>
2016-01-23 04:18:42 -0700

    PEPE8

--- server/webapp/util.py
+++ server/webapp/util.py
@@ -2,6 +2,6 @@ def verify_structure(obj, fields):
     if not obj:
         return False
     for field in fields:
-	if not field in obj:
-	    return False
+        if field not in obj:
+            return False
     return True
