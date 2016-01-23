e064d5a378f7960ba87700e8c0ca92d57b288901
Steve King, Jr <stevekx86@gmail.com>
2016-01-23 02:48:07 -0700

    Next on Python's Top Model

--- server/webapp/models.py
+++ server/webapp/models.py
@@ -1,68 +1,20 @@
-from __init__ import db
+from __init__ import redis_db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-class User(db.Model):
-    __tablename__ = "user"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True, index=True)
-    username = db.Column(db.String(64), index=True)
-    phone_number = db.Column(db.String(15))
-    password_hash = db.Column(db.String(120))
-    warranty = db.Column(db.Boolean, default=False)
-    incidents = db.relationship("Incident", backref="user")
-
-    def __init__(self, username, phone, password, warranty):
+class User(object):
+    def __init__(self, username, password, phone_number):
         self.username = username
-        self.phone_number = phone
-        self.password_hash = generate_password_hash(password, "pbkdf2:sha256:10000")
-        self.warranty = warranty
+        self.password_hash = generate_password_hash(password, method="pbkdf2:sha256", salt_length=32)
+        self.phone_number = phone_number
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
 
+    def write_to_db(self):
+        user_dict = {"password_hash": self.password_hash, "phone_number": self.phone_number}
+        redis_db.hmset(self.username, user_dict)
 
-class Incident(db.Model):
-    __tablename__ = "incident"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    real = db.Column(db.Boolean, default=True)
-    frames = db.relationship("AccelerationFrame", backref="incident")
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-
-    def __init__(self, real, user):
-        self.real = real
-        self.user_id = user.id
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "user": self.user.phone_number,
-            "real": self.real,
-            "frames": [each.serialize() for each in self.frames]
-        }
-
-
-class AccelerationFrame(db.Model):
-    __tablename__ = "frame"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    batch_order = db.Column(db.Integer)
-    accel_x = db.Column(db.Float)
-    accel_y = db.Column(db.Float)
-    accel_z = db.Column(db.Float)
-    incident_id = db.Column(db.Integer, db.ForeignKey("incident.id"))
-
-    def __init__(self, batch_order, accel_x, accel_y, accel_z, incident):
-        self.batch_order = batch_order
-        self.accel_x = accel_x
-        self.accel_y = accel_y
-        self.accel_z = accel_z
-        self.incident_id = incident.id
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "accel_x": self.accel_x,
-            "accel_y": self.accel_y,
-            "accel_z": self.accel_z,
-            "incident_id": self.incident_id
-        }
-
+    @classmethod
+    def get_from_db(cls, username):
+        redis_db.hmget(username, ["password_hash", "phone_number"])
